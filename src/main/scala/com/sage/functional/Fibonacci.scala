4660048d6fdebaac4505a68ab8f3f1c21ef2ff40
Mark Sage <masage81@gmail.com>
2017-03-08 20:49:56 -0600

    more stuff

---
+++ src/main/scala/com/sage/functional/Fibonacci.scala
@@ -0,0 +1,28 @@
+package com.sage.functional
+
+import scala.annotation.tailrec
+
+/**
+  * Created by marksage on 3/8/17.
+  */
+object Fibonacci extends App{
+ // 0,\;1,\;1,\;2,\;3,\;5,\;8,\;13,\;21,\;34,\;55,\;89,\;144,\;\ldots \;.[3]
+ def fib(x: Int): Int = {
+  def fibHelper(x:Int, prev:Int = 0, next:Int = 1):Int ={
+    x match {
+      case 0 => prev
+      case 1 => next
+      case x => fibHelper(x - 1, next, next + prev)
+    }
+  }
+   fibHelper(x)
+ }
+
+  println(fib(1))
+  println(fib(2))
+  println(fib(3))
+  println(fib(4))
+  println(fib(5))
+  println(fib(6))
+
+}
